835586097555923d988ab6d2f65e7068d43fa213
chenJz1012 <597160667@qq.com>
2017-03-15 14:46:56 +0800

    fix

--- src/main/java/com/topie/animal/task/GenerateReportTask.java
+++ src/main/java/com/topie/animal/task/GenerateReportTask.java
@@ -1,7 +1,11 @@
 package com.topie.animal.task;
 
 import com.topie.animal.constant.ReportTypeE;
+import com.topie.animal.dto.WeekDto;
 import com.topie.animal.service.IReportService;
+import com.topie.animal.service.IWeekConfigService;
+import com.topie.animal.util.BeginTimeUtil;
+import com.topie.common.utils.date.DateStyle;
 import com.topie.common.utils.date.DateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +14,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.List;
 
 /**
  * Created by chenguojun on 2017/2/4.
@@ -21,6 +26,20 @@ public class GenerateReportTask {
 
     @Autowired
     private IReportService iReportService;
+
+    @Autowired
+    private IWeekConfigService iWeekConfigService;
+
+    @Scheduled(cron = "10 0 0 * * ?")
+    public void weekGenerate() {
+        logger.info("开始生成");
+        Date today = DateUtil.getToday();
+        List<WeekDto> list = iWeekConfigService.getDays(today);
+        for (WeekDto weekDto : list) {
+            Date beginTime = DateUtil.StringToDate(weekDto.getTime(), DateStyle.YYYY_MM_DD);
+            if (beginTime.equals(today)) iReportService.insertReport(ReportTypeE.WEEK.getCode(), beginTime);
+        }
+    }
 
     @Scheduled(cron = "10 0 0 1 * ?")
     public void monthGenerate() {
@@ -34,5 +53,12 @@ public class GenerateReportTask {
         logger.info("开始生成");
         Date beginTime = DateUtil.getCurrentMonthFirstDay();
         iReportService.insertReport(ReportTypeE.SEASON.getCode(), beginTime);
+    }
+
+    @Scheduled(cron = "10 0 0 1 3,9 ?")
+    public void halfYearGenerate() {
+        logger.info("开始生成");
+        Date beginTime = BeginTimeUtil.getCurrentHalfYearBeginTime();
+        iReportService.insertReport(ReportTypeE.HALF_YEAR.getCode(), beginTime);
     }
 }
