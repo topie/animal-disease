21a43ec684ba4b9e0379b1b9bac6626e48a7772c
chenJz1012 <597160667@qq.com>
2017-06-21 17:50:42 +0800

    fix

--- src/main/java/com/topie/animal/service/impl/ReportServiceImpl.java
+++ src/main/java/com/topie/animal/service/impl/ReportServiceImpl.java
@@ -77,7 +77,6 @@ public class ReportServiceImpl extends BaseService<Report> implements IReportSer
         }
         List<String> templateIdList = iTemplateService.selectIdsByReportType(reportType);
         for (String templateId : templateIdList) {
-            int totalCnt = 0;
             for (String reportUserId : userIdList) {
                 Report report = new Report();
                 report.setTemplateId(templateId);
@@ -85,18 +84,17 @@ public class ReportServiceImpl extends BaseService<Report> implements IReportSer
                 report.setBeginTime(beginTime);
                 report.setReportUserId(reportUserId);
                 int cnt = reportMapper.selectCount(report);
-                totalCnt += cnt;
-            }
-            if (totalCnt == 0 && userIdList.size() > 0) {
-                Report report = new Report();
-                report.setTemplateId(templateId);
-                report.setReportType(reportType);
-                report.setBeginTime(beginTime);
-                report.setReportUserId(userIdList.get(0));
-                report.setStatus(0);
-                report.setEndTime(DateUtil.addDay(beginTime, 9));
-                report.setReportId(UUIDUtil.getUUID());
-                reportMapper.insertSelective(report);
+                if (cnt == 0) {
+                    Report fR = new Report();
+                    fR.setTemplateId(templateId);
+                    fR.setReportType(reportType);
+                    fR.setBeginTime(beginTime);
+                    fR.setReportUserId(userIdList.get(0));
+                    fR.setStatus(0);
+                    fR.setEndTime(DateUtil.addDay(beginTime, 9));
+                    fR.setReportId(UUIDUtil.getUUID());
+                    reportMapper.insertSelective(fR);
+                }
             }
         }
         return 1;
